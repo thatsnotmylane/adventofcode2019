d2672a09bc79894a8ef1ee387198c87e7286e2cc
thatsnotmylane <thatsnotmylane@gmail.com>
2020-12-04 13:28:04 -0700

    make the app

---
+++ adventofcode2019/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace adventofcode2019
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            while(true)
+            {
+
+            }
+        }
+    }
+}
